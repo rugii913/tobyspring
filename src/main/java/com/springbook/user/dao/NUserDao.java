97de1b5e6ca7127e1d551e3bb7e18054a53aed3c
rugii913 <rugii913@gmail.com>
2023-06-29 16:35:58 +0900

    p.72 리스트 1-6,7 / DB 커넥션 부분을 별도의 클래스로 분리
    ㅁSimpleConnectionMaker - DB 커넥션 부분을 별도의 클래스로 분리
     ㅇmakeNewConnection() 기존의 getConnection 메서드
    
    ㅁUserDao abstract 부분 삭제
    
    ***** 문제점
    UserDao의 코드가 SimpleConnectionMaker라는 특정 클래스에 종속되어 있으므로
     - 상속을 사용했을 때처럼 UserDao 코드의 수정 없이 DB 커넥션 생성 기능을 변경할 방법이 없다.
     -> NUserDao, DUserDao를 쓸 수가 없음

--- src/main/java/com/springbook/user/dao/NUserDao.java
+++ src/main/java/com/springbook/user/dao/NUserDao.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class NUserDao extends UserDao{
-    @Override
+public class NUserDao /*extends UserDao*/{
+    /*@Override*/
     public Connection getConnection() throws ClassNotFoundException, SQLException {
+        //~~~~~~~~~~~~~~~~~~~~~~~~
         //N사 DB connection 생성코드
+        //~~~~~~~~~~~~~~~~~~~~~~~~
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
         return c;
--- src/main/java/com/springbook/user/dao/SimpleConnectionMaker.java
+++ src/main/java/com/springbook/user/dao/SimpleConnectionMaker.java
@@ -4,12 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DUserDao /*extends UserDao*/{
-    /*@Override*/
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        //~~~~~~~~~~~~~~~~~~~~~~~~
-        //D사 DB connection 생성코드
-        //~~~~~~~~~~~~~~~~~~~~~~~~
+public class SimpleConnectionMaker { //상속을 이용한 확장 방식을 사용할 필요가 없으므로 추상 클래스로 만들 필요가 없다.
+
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
         return c;
