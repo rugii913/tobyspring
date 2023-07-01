1189cf3af7c5e7bae50889b410eea705d2ba3f22
rugii913 <rugii913@gmail.com>
2023-07-01 12:21:57 +0900

    p.169 2-11 getCount() 더 꼼꼼한 테스트
    * 두 개의 테스트가 어떤 순서로 실행될지 알 수 없음
     => 모든 테스트는 실행 순서에 상관없이 독립적으로 항상 동일한 결과를 낼
     수 있도록 해야함

--- src/test/java/com/springbook/user/dao/UserDaoTest.java
+++ src/test/java/com/springbook/user/dao/UserDaoTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -30,6 +31,28 @@ class UserDaoTest {
 
         assertThat(user2.getName()).isEqualTo(user.getName());
         assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+
+    @Test
+    public void count() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount()).isEqualTo(0);
+
+        dao.add(user1);
+        assertThat(dao.getCount()).isEqualTo(1);
+
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
+        
+        dao.add(user3);
+        assertThat(dao.getCount()).isEqualTo(3);
     }
 
 }
