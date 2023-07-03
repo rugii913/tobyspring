2b1d19fdf8ed96439767a0f7abddb59902655a3c
rugii913 <rugii913@gmail.com>
2023-07-03 17:37:26 +0900

    p.203 2-26,27 (학습 테스트) 컨텍스트 테스트 / deprecated 대체
    * @ContextConfiguration(locations = "/junit.xml") 지정해줘야 함
    * assertj 테스트 메서드로 대체한 것들 있음

--- src/test/java/com/springbook/learningtest/JUnitTest.java
+++ src/test/java/com/springbook/learningtest/JUnitTest.java
@@ -1,23 +1,45 @@
 package com.springbook.learningtest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/junit.xml")
 public class JUnitTest {
+
+    @Autowired
+    ApplicationContext context;
+    // 테스트 컨텍스트가 매번 주입해주는 어플리케이션 컨텍스트는 항상 같은 객체인지 테스트로 확인해본다.
 
     static JUnitTest testObject;
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
-    public void test1() {
+    public void test1() { // 시간날 때 assertj 살펴보고 테스트 발전시켜 보기
         assertThat(this).isNotSameAs(testObject);
         assertThat(this).isNotIn(testObjects);
         testObject = this;
         testObjects.add(this);
+
+        assertThat(contextObject).satisfiesAnyOf(
+                contextObject -> assertThat(contextObject).isNull(),
+                contextObject -> assertThat(contextObject).isEqualTo(this.context)
+        );
+        contextObject = this.context;
+
+        System.out.println(this.context);
+        System.out.println(this.context);
+        System.out.println(this.context);
     }
 
     @Test
@@ -26,6 +48,12 @@ public class JUnitTest {
         assertThat(this).isNotIn(testObjects);
         testObject = this;
         testObjects.add(this);
+
+        assertThat(contextObject).satisfiesAnyOf(
+                contextObject -> assertThat(contextObject).isEqualTo(null),
+                contextObject -> assertThat(contextObject).isEqualTo(this.context)
+        );
+        contextObject = this.context;
     }
 
     @Test
@@ -34,5 +62,11 @@ public class JUnitTest {
         assertThat(this).isNotIn(testObjects);
         testObject = this;
         testObjects.add(this);
+
+        assertThat(contextObject).satisfiesAnyOf(
+                contextObject -> assertThat(contextObject).isNull(),
+                contextObject -> assertThat(contextObject).isEqualTo(this.context)
+        );
+        contextObject = this.context;
     }
 }
