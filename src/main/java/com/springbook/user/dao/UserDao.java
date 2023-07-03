2d3691d42eec8a9b5d2b9e92d2f76f1c89bdddd5
rugii913 <rugii913@gmail.com>
2023-07-03 18:18:56 +0900

    p.211 3-1,2 deleteAll() 예외 발생 시에도 리소스 반환하도록 예외 처리

--- src/main/java/com/springbook/user/dao/UserDao.java
+++ src/main/java/com/springbook/user/dao/UserDao.java
@@ -62,13 +62,38 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection();
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        ps.executeUpdate();
+//        PreparedStatement ps = c.prepareStatement("delete from users");
+//        ps.executeUpdate(); // 여기서 예외가 발생하면 메서드 실행 중단 -> 리소스 반환되지 않음
+        try { // 예외가 발생할 가능성이 있는 코드를 모두 try 블록으로 묶어준다.
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다.
+            // 아직은 예외를 다시 메서드 밖으로 던지기만 함
+            throw e;
+        } finally { // finally이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    // ps.close() 메서드에서도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
+                    // 그렇지 않으면 Connection을 close() 하지 못하고 메서드를 빠져나갈 수 있다.
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close(); // -> Connection 반환
+                } catch (SQLException e) {
+                }
+            }
+        }
 
-        ps.close();
-        c.close();
+
+
     }
 
     public int getCount() throws SQLException {
