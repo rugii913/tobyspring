ce7e45d87832c2057c445b26e245b4c37791b4ba
rugii913 <rugii913@gmail.com>
2023-07-03 13:29:42 +0900

    p.180 2-15 테스트 중복 코드 제거하여 개선(@BeforeEach) / @Before 없어짐
    * JUnit @Before 없어져서 @BeforeEach로 대체

--- src/test/java/com/springbook/user/dao/UserDaoTest.java
+++ src/test/java/com/springbook/user/dao/UserDaoTest.java
@@ -1,25 +1,30 @@
 package com.springbook.user.dao;
 
 import com.springbook.user.domain.User;
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class UserDaoTest {
+class UserDaoTest { // UserDaoTest를 Bean으로 만들지는 않았기 때문에 자동으로 주입받지 못하고, context에서 불러옴
+
+    private UserDao dao; // setUp() 메서드에서 만드는 객체를 테스트 메서드에서 사용할 수 있도록 인스턴스 변수로 선언
+
+    @BeforeEach // JUnit 제공 어노테이션, @Test 메서드가 실행되기 전에 먼저 실행돼야 하는 메서드를 정의
+    public void setUp() { // 각 테스트 메서드에 반복적으로 나타났던 dao를 가져오는 코드를 제거하고 별도의 메서드로 추출
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        this.dao = context.getBean("userDao", UserDao.class);
+    }
 
     @Test
     void addAndGet() throws SQLException {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class); // 중복되지 않는 값을 가진 두 User 오브젝트를 준비
+        // 중복되지 않는 값을 가진 두 User 오브젝트를 준비
         User user1 = new User("gyumee", "박성철", "springno1");
         User user2 = new User("leegw700", "이길원", "springno2");
 
@@ -44,9 +49,6 @@ class UserDaoTest {
 
     @Test
     public void count() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
         User user1 = new User("gyumee", "박성철", "springno1");
         User user2 = new User("leegw700", "이길원", "springno2");
         User user3 = new User("bumjin", "박범진", "springno3");
@@ -66,9 +68,6 @@ class UserDaoTest {
 
     @Test
     public void getUserFailure() throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
         dao.deleteAll();
         assertThat(dao.getCount()).isEqualTo(0);
 
