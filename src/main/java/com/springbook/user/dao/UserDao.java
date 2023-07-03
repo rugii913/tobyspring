4c934249099c53f2ca5c4763f4b4da25c5fa6edf
rugii913 <rugii913@gmail.com>
2023-07-03 22:45:20 +0900

    p.244 3-26,27 콜백의 분리와 재활용
    ㅁ콜백 중 변하지 않는 부분을 또 찾아내어 분리
     ㅇ바인딩할 파라미터가 없는 경우라면 재활용 가능

--- src/main/java/com/springbook/user/dao/UserDao.java
+++ src/main/java/com/springbook/user/dao/UserDao.java
@@ -65,11 +65,12 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
+        executeSql("delete from users"); // 변하는 SQL 문장
+    }
 
-        this.jdbcContext.workWithStatementStrategy(
-                (connection) -> {
-                    return connection.prepareStatement("delete from users");
-                }
+    private void executeSql(final String query) throws SQLException { // 바인딩할 파라미터 없이 SQL만 전달하면 되는 경우
+        this.jdbcContext.workWithStatementStrategy( // 변하지 않는 부분: 어떤 query를 넣은 람다식을 인자로 전달하여 메서드 실행
+                (connection) -> connection.prepareStatement(query)
         );
     }
 
