11a07694206ff6e23fe060b3d4f95091e1485f74
rugii913 <rugii913@gmail.com>
2023-07-05 20:17:26 +0900

    p.358 5-38~40 upgradeLevels()가 가져야할 트랜잭션 경계설정 구조

--- src/test/java/com/springbook/learningtest/transaction/TransactionExample.java
+++ src/test/java/com/springbook/learningtest/transaction/TransactionExample.java
@@ -62,4 +62,19 @@ public class TransactionExample {
 
         Assertions.assertThat(dao.getAll().size()).isEqualTo(0);
     }
+
+    // 5-38 upgradeLevels()가 가져야할 트랜잭션 경계설정 구조
+    public void upgradeLevels() throws Exception {
+        // (1) DB Connection 생성
+        // (2) 트랜잭션 시작
+        try {
+            // (3) DAO 메서드 호출
+            // (4) 트랜잭션 커밋
+        } catch (Exception e) {
+            // (5) 트랜잭션 롤백
+            throw e;
+        } finally {
+            // (6) DB Connection 종료
+        }
+    }
 }
