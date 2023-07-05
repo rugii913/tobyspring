faca9e80f8ecbbe6dcd5076a69e14bed2ef0c077
rugii913 <rugii913@gmail.com>
2023-07-05 14:15:49 +0900

    p.331 5.16,17 UserService 빈 주입 테스트

---
+++ src/test/java/com/springbook/user/service/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.springbook.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
+class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void bean() {
+        assertThat(this.userService).isNotNull();
+    }
+}
