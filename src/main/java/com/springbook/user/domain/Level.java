8d2af2390e8b2b60cd9faa5cb787a9a2a9667f25
rugii913 <rugii913@gmail.com>
2023-07-05 15:50:10 +0900

    p.340 5-26 upgradeLevel() 분리 작업1 - Level enum 수정

--- src/main/java/com/springbook/user/domain/Level.java
+++ src/main/java/com/springbook/user/domain/Level.java
@@ -1,16 +1,22 @@
 package com.springbook.user.domain;
 
 public enum Level {
-    BASIC(1), SILVER(2), GOLD(3); // enum 객체 3개 정의
+    GOLD(3, null), SILVER(2, GOLD), BASIC(1, SILVER) ; // enum 선언에 DB에 저장할 값과 함께 다음 단계의 레벨 정보도 추가한다.
 
-    private final int value;
+    private final int value; // DB에 저장할 값
+    private final Level next; // 다음 단계 레벨 정보를 스스로 갖고 있도록 Level 타입 next 변수 추가
 
-    Level(int value) { // DB에 저장할 값을 넣어줄 생성자를 만들어둔다.
+    Level(int value, Level next) {
         this.value = value;
+        this.next = next;
     }
 
     public int intValue() { // 값을 가져오는 메서드
         return value;
+    }
+
+    public Level nextLevel() {
+        return this.next;
     }
 
     public static Level valueOf(int value) { // 값으로부터 Level 타입 객체를 가져오도록 만든 스태틱 메서드
