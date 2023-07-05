426e09d882bfe17b602ca488a8c015f7d8c0c21a
rugii913 <rugii913@gmail.com>
2023-07-05 16:17:40 +0900

    p.344 5-29 User 테스트

---
+++ src/test/java/com/springbook/user/service/UserTest.java
@@ -0,0 +1,44 @@
+package com.springbook.user.service;
+
+import com.springbook.user.domain.Level;
+import com.springbook.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UserTest {
+
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            user.setLevel(level);
+            user.upgradeLevel();
+            assertThat(user.getLevel()).isEqualTo(level.nextLevel());
+        }
+    }
+
+    @Test
+    public void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null) {
+                continue;
+            }
+            user.setLevel(level);
+            assertThatThrownBy(() -> user.upgradeLevel()).isExactlyInstanceOf(IllegalStateException.class);
+        }
+    }
+}
