ee658f3cdde63bea117b7b45b8dbe98b1fe6b618
rugii913 <rugii913@gmail.com>
2023-07-05 14:42:01 +0900

    p.333 5-19,20 픽스처 작성, upgradeLevels() 테스트
    ? 테스트 클래스에 userDao 없어서 userService.userDao 이렇게 불러왔음

--- src/test/java/com/springbook/user/service/UserServiceTest.java
+++ src/test/java/com/springbook/user/service/UserServiceTest.java
@@ -1,10 +1,16 @@
 package com.springbook.user.service;
 
+import com.springbook.user.domain.Level;
+import com.springbook.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,4 +21,37 @@ class UserServiceTest {
 
     @Autowired
     UserService userService;
+    List<User> users; // 테스트 픽스처
+
+    @BeforeEach
+    public void setUp() {
+        users = Arrays.asList( // 배열을 리스트로 만들어주는 편리한 메서드. 배열을 가변인자로 넣어주면 더욱 편리하다.
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userService.userDao.deleteAll();
+        for (User user : users) {
+            userService.userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userService.userDao.get(user.getId());
+        assertThat(userUpdate.getLevel()).isEqualTo(expectedLevel);
+    }
 }
