19c4fee8d2f66637eb69b603ee73bede6b734af6
rugii913 <rugii913@gmail.com>
2023-07-05 15:14:19 +0900

    p.339 5-24,25 업그레이드 가능 판별 메서드 -> 업그레이드 작업 메서드 작성

--- src/main/java/com/springbook/user/service/UserService.java
+++ src/main/java/com/springbook/user/service/UserService.java
@@ -46,6 +46,29 @@ public class UserService {
         }
     }
 
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel) { // 레벨별로 구분해서 조건을 판단
+            case BASIC: return (user.getLogin() >= 50);
+            case SILVER: return (user.getRecommend() >= 30);
+            case GOLD: return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+            // -> 현재 로직에서 다룰 수 없는 레벨이 주어지면 예외를 발생시킨다. 새로운 레벨이 추가되고 로직을 수정하지 않으면 에러가 나서 확인할 수 있다.
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        // 작업 1. 사용자의 레벨을 다음 단계로 바꿔 줌
+        if (user.getLevel() == Level.BASIC) {
+            user.setLevel(Level.SILVER);
+        } else if (user.getLevel() == Level.SILVER) {
+            user.setLevel(Level.GOLD);
+        }
+        
+        // 작업 2. 변경사항을 DB에 업데이트
+        userDao.update(user);
+    }
+
     public void add(User user) {
         if (user.getLevel() == null) {
             user.setLevel(Level.BASIC);
