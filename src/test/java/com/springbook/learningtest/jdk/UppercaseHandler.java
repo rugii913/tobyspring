56abba61e07a6164aecf521e98ad8f7d3dbab853
rugii913 <rugii913@gmail.com>
2023-07-06 20:52:14 +0900

    p.445 6-25,26 다이나믹 프록시 확장(target 타입 제한 x, 다른 조건 추가)

--- src/test/java/com/springbook/learningtest/jdk/UppercaseHandler.java
+++ src/test/java/com/springbook/learningtest/jdk/UppercaseHandler.java
@@ -5,16 +5,20 @@ import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
 
-    Hello target;
+    Object target;
 
-    public UppercaseHandler(Hello target) {
-        this.target = target; // 다이나믹 프록시로부터 전달받은 요청을 다시 target 객체에 위임해야 하므로 target 객체를 주입받아 둔다.(생성자 주입)
+    public UppercaseHandler(Object target) {
+        this.target = target; // 어떤 종류의 인터페이스를 구현한 target에도 적용 가능하도록 Object 타입으로 수정
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String) method.invoke(target, args); // -> target으로 위임, 인터페이스의 메서드 호출에 모두 적용된다.
-        // -> toUpperCase()를 적용하기 위해 (String) 캐스팅함. Hello의 모든 메서드의 반환 타입은 String이므로 괜찮다.
-        return ret.toUpperCase(); // -> 부가기능 제공
+        Object ret = (String) method.invoke(target, args);
+        if (ret instanceof String && method.getName().startsWith("say")) {
+            // -> 호출한 메서드의 리턴 타입이 String인 경우 && 메서드 이름이 say로 시작하는 경우 둘 다 만족할 때 부가기능 적용
+            return ((String) ret).toUpperCase();
+        } else { // 조건이 일치하지 않으면 target의 method 호출 결과를 그대로 반환
+            return ret;
+        }
     }
 }
