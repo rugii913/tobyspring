bb20e9d47f16875c98e0de73e9f03e186c4eb7ea
rugii913 <rugii913@gmail.com>
2023-07-06 21:08:30 +0900

    p.446 6-27 트랜잭션에 다이나믹 프록시 적용 - InvocationHandler 구현
    * UserServiceTx 코드와 거의 비슷한데 차이점
     - 예외는 RuntimeException 대신 InvocationTargetException을 잡아야 함
     - 리플렉션 메서드인 Method.invoke()를 이용해 target 구현체의 메서드를 호출할 때 터지는 예외는 InvocationTargetException으로 한 번 포장돼서 전달됨
     - e.getTargetException() 으로 중첩되어 있는 예외를 가져와야 함

---
+++ src/main/java/com/springbook/proxy/TransactionHandler.java
@@ -0,0 +1,49 @@
+package com.springbook.proxy;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TransactionHandler implements InvocationHandler {
+    private Object target; // -> target, 어떤 타입이라도 가능하도록 Object로 함.
+    private PlatformTransactionManager transactionManager; // -> 트랜잭션 기능을 제공하는 데 필요한 트랜잭션 매니저
+    private String pattern; // -> 트랜잭션을 적용할 메서드 이름 패턴
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.getName().startsWith(pattern)) { // 트랜잭션 적용 대상 메서드를 선별해서 트랜잭션 경계설정 기능을 부여해준다.
+            return invokeInTransaction(method, args);
+        } else {
+            return method.invoke(target, args);
+        }
+    }
+
+    private Object invokeInTransaction(Method method, Object[] args) throws Throwable {
+        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try { // 트랜잭션을 시작하고 target 객체의 메서드를 호출, 예외가 발생하지 않으면 커밋, 발생하면 롤백
+            Object ret = method.invoke(target, args);
+            this.transactionManager.commit(status);
+            return ret;
+        } catch (InvocationTargetException e) {
+            this.transactionManager.rollback(status);
+            throw e.getTargetException();
+        }
+    }
+}
