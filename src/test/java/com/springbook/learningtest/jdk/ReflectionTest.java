9c94ccf135b0c5ca24b335069d359336502c91a6
rugii913 <rugii913@gmail.com>
2023-07-06 18:26:41 +0900

    p.438 6-17 java.lang.reflect.Method를 이용한 메서드 호출 학습 테스트

---
+++ src/test/java/com/springbook/learningtest/jdk/ReflectionTest.java
@@ -0,0 +1,27 @@
+package com.springbook.learningtest.jdk;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+
+    @Test
+    public void invokeMethod() throws Exception {
+        String name = "Spring";
+
+        // length()
+        assertThat(name.length()).isEqualTo(6);
+
+        Method lengthMethod = String.class.getMethod("length"); // 메서드 이름이 length이고 파라미터는 없는 것
+        assertThat((Integer) lengthMethod.invoke(name)).isEqualTo(6);
+
+        // charAt()
+        assertThat(name.charAt(0)).isEqualTo('S');
+
+        Method charAtMethod = String.class.getMethod("charAt", int.class); // 메서드 이름이 charAt이고 파라미터가 int 딱 한 개인 것
+        assertThat((Character) charAtMethod.invoke(name, 0)).isEqualTo('S');
+    }
+}
