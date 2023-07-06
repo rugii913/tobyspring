351e00cbd190b8808221013c58a4aa35537f00f6
rugii913 <rugii913@gmail.com>
2023-07-06 15:17:52 +0900

    UserDao update()가 int 반환하는 부분 수정
    * MockUserDao 만들면서 충돌

--- src/main/java/com/springbook/user/dao/UserDao.java
+++ src/main/java/com/springbook/user/dao/UserDao.java
@@ -10,5 +10,5 @@ public interface UserDao {
     List<User> getAll();
     void deleteAll();
     Integer getCount();
-    int update(User user);
+    void update(User user);
 }
--- src/main/java/com/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/com/springbook/user/dao/UserDaoJdbc.java
@@ -50,8 +50,8 @@ public class UserDaoJdbc implements UserDao {
     }
 
     @Override
-    public int update(User user) {
-        return this.jdbcTemplate.update(
+    public void update(User user) {
+        this.jdbcTemplate.update(
                 "update users set name = ?, password = ?, level = ?, login = ?, recommend = ?, email = ? where id = ?",
                 user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId()
         );
