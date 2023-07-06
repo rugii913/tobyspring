199d2649167f3780397d6b4d4d4e415bf4274934
rugii913 <rugii913@gmail.com>
2023-07-06 14:23:49 +0900

    p.408 UserServiceTx에 트랜잭션 적용

--- src/main/java/com/springbook/user/service/UserServiceTx.java
+++ src/main/java/com/springbook/user/service/UserServiceTx.java
@@ -1,13 +1,21 @@
 package com.springbook.user.service;
 
 import com.springbook.user.domain.User;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class UserServiceTx implements UserService {
 
     UserService userService; // UserService 구현체를 DI 받는다.
+    PlatformTransactionManager transactionManager;
 
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
     }
 
     // DI 받은 UserService 구현체에 모든 기능을 위임한다.
@@ -18,6 +26,15 @@ public class UserServiceTx implements UserService {
 
     @Override
     public void upgradeLevels() {
-        userService.upgradeLevels();
+        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            userService.upgradeLevels();
+
+            this.transactionManager.commit(status);
+        } catch (RuntimeException e) {
+            this.transactionManager.rollback(status);
+            throw e;
+        }
     }
 }
