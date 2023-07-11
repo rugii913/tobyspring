b1ee820f603e156023575eb7aff7db31c06be57d
rugii913 <rugii913@gmail.com>
2023-07-11 18:24:12 +0900

    p.538 6-86~88 UserService에 @Transactional 적용

--- src/main/java/com/springbook/user/service/UserService.java
+++ src/main/java/com/springbook/user/service/UserService.java
@@ -1,12 +1,16 @@
 package com.springbook.user.service;
 
 import com.springbook.user.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional // 아무런 속성 엘리먼트를 지정하지 않으면 디폴트 트랜잭션 속성 값 적용
 public interface UserService {
     void add(User user);
+    @Transactional(readOnly = true) // 대체 정책에 의해 우선 적용
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void update(User user);
