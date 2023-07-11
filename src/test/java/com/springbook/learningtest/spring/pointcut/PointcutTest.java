38a2f822bc0435b4e20f6e9bb35f7ce8d40850f4
rugii913 <rugii913@gmail.com>
2023-07-11 13:46:27 +0900

    p.494 6-62~64 포인트컷 표현식 테스트 및 헬퍼 메서드 2개

--- src/test/java/com/springbook/learningtest/spring/pointcut/PointcutTest.java
+++ src/test/java/com/springbook/learningtest/spring/pointcut/PointcutTest.java
@@ -30,4 +30,28 @@ public class PointcutTest {
                 .matches(Target.class.getMethod("method"), null))
                 .isEqualTo(false);
     }
+
+    @Test
+    public void pointcut() throws Exception {
+        targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+    }
+
+    public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName,
+                                Class<?>... args) throws Exception {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+
+        assertThat(pointcut.getClassFilter().matches(clazz)
+                && pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null))
+                .isEqualTo(expected);
+    }
+
+    public void targetClassPointcutMatches(String expression, boolean... expected) throws Exception {
+        pointcutMatches(expression, expected[0], Target.class, "hello");
+        pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+        pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+        pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+        pointcutMatches(expression, expected[4], Target.class, "method");
+        pointcutMatches(expression, expected[5], Bean.class, "method");
+    }
 }
