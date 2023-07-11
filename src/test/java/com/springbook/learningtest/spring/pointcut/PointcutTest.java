e2507fa5e7537d206c4eb6c0d5f4f473f94cfde6
rugii913 <rugii913@gmail.com>
2023-07-11 12:56:26 +0900

    p.492 6-61 포인트컷 표현식 테스트 * 테스트 의존성 추가
    * testImplementation 'org.springframework.boot:spring-boot-starter-aop'

--- src/test/java/com/springbook/learningtest/spring/pointcut/PointcutTest.java
+++ src/test/java/com/springbook/learningtest/spring/pointcut/PointcutTest.java
@@ -1,4 +1,33 @@
 package com.springbook.learningtest.spring.pointcut;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PointcutTest {
+
+    @Test
+    public void methodSignaturePointcut() throws SecurityException, NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(public int com.springbook.learningtest.spring.pointcut.Target.minus(int, int) throws java.lang.RuntimeException)");
+
+        // Target.minus()
+        assertThat(pointcut.getClassFilter().matches(Target.class)
+                && pointcut.getMethodMatcher()
+                .matches(Target.class.getMethod("minus", int.class, int.class), null))
+                .isEqualTo(true);
+
+        // Target.plus()
+        assertThat(pointcut.getClassFilter().matches(Target.class)
+                && pointcut.getMethodMatcher()
+                .matches(Target.class.getMethod("plus", int.class, int.class), null))
+                .isEqualTo(false);
+
+        // Bean.method()
+        assertThat(pointcut.getClassFilter().matches(Target.class)
+                && pointcut.getMethodMatcher()
+                .matches(Target.class.getMethod("method"), null))
+                .isEqualTo(false);
+    }
 }
