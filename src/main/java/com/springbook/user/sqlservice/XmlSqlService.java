b9e37d274fc36881cbce51d16a71e1fe44671a79
rugii913 <rugii913@gmail.com>
2023-07-28 17:00:48 +0900

    p.577 7-21~26 생성자 대신 @PostConstruct 사용한 빈 초기화 및 그 설정

--- src/main/java/com/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/com/springbook/user/sqlservice/XmlSqlService.java
@@ -4,6 +4,7 @@ import com.springbook.user.dao.UserDao;
 import com.springbook.user.sqlservice.jaxb.SqlType;
 import com.springbook.user.sqlservice.jaxb.Sqlmap;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -13,23 +14,12 @@ import java.util.Map;
 
 public class XmlSqlService implements SqlService {
 
+    private String sqlmapFile;
     private Map<String, String> sqlMap = new HashMap<>(); // => 읽어온 SQL을 저장해둘 맵
 
-    public XmlSqlService() { // 스프링이 객체를 만드는 시점에서 SQL을 읽어오도록 생성자를 이용한다.
-        // JAXB API를 이용해 XML 문서를 오브젝트 트리로 읽어온다.
-        String contextPath = Sqlmap.class.getPackage().getName();
-        try {
-            JAXBContext context = JAXBContext.newInstance(contextPath);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml"); // => UserDao와 같은 클래스패스의 sqlmap.xml 파일을 변환한다.
-            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
-            for (SqlType sql : sqlmap.getSql()) { // 읽어온 SQL을 맵으로 저장해둔다.
-                sqlMap.put(sql.getKey(), sql.getValue());
-            }
-        } catch (JAXBException e) {
-            throw new RuntimeException(e); // => JAXBException은 복구 불가능한 예외다. 불필요한 throws를 피하도록 런타임 예외로 포장해서 던진다.
-        }
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
     }
 
     @Override
@@ -40,6 +30,24 @@ public class XmlSqlService implements SqlService {
             throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
         } else {
             return sql;
+        }
+    }
+    
+    @PostConstruct // -> loadSql() 메서드를 빈의 초기화 메서드로 지정
+    public void loadSql() { // 생성자에서 초기화하는 대신 사용
+        // JAXB API를 이용해 XML 문서를 오브젝트 트리로 읽어온다.
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile); // => UserDao와 같은 클래스패스의 sqlmap.xml 파일을 변환한다.
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql : sqlmap.getSql()) { // 읽어온 SQL을 맵으로 저장해둔다.
+                sqlMap.put(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e); // => JAXBException은 복구 불가능한 예외다. 불필요한 throws를 피하도록 런타임 예외로 포장해서 던진다.
         }
     }
 }
