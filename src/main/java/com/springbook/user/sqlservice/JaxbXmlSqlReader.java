d20efeb461ac4c14903c0193aa8fa8b82cc35cc6
rugii913 <rugii913@gmail.com>
2023-07-29 19:17:09 +0900

    p.591 7-37~40 reader, registry 독립 파일로 분리 및 빈 설정

---
+++ src/main/java/com/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -0,0 +1,36 @@
+package com.springbook.user.sqlservice;
+
+import com.springbook.user.dao.UserDao;
+import com.springbook.user.sqlservice.jaxb.SqlType;
+import com.springbook.user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
+public class JaxbXmlSqlReader implements SqlReader {
+
+    // sqlmapFile은 SqlReader의 특정 구현 방법에 종속되는 프로퍼티가 된다.
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream(sqlmapFile);
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
