95ba9a99458262d5c704560753b90f5cca1c2ee8
rugii913 <rugii913@gmail.com>
2023-08-01 17:09:27 +0900

    p.679 7-106 관련, 메타 어노테이션과 @Repository

--- src/main/java/com/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/com/springbook/user/dao/UserDaoJdbc.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
 
-@Component
+@Repository
 public class UserDaoJdbc implements UserDao {
 
     private RowMapper<User> userMapper = (rs, rowNum) -> {
